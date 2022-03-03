7fa899f0096ac202daaa41c2333e0d03ae7556bf
praveenraj2001 <nyalam.praveenraj@gmail.com>
2022-03-03 19:54:12 +0530

    commit

--- Project/3_Implementation/src/test_project.c
+++ Project/3_Implementation/src/test_project.c
@@ -12,6 +12,11 @@ void tearDown()
 void test_map(void) 
 { 
   TEST_ASSERT_EQUAL_FLOAT(100,fan_led_percent_map(252));
+  TEST_ASSERT_EQUAL_FLOAT(75,fan_led_percent_map(189));
+  TEST_ASSERT_EQUAL_FLOAT(50,fan_led_percent_map(126)); 
+  TEST_ASSERT_EQUAL_FLOAT(25,fan_led_percent_map(63));
+  TEST_ASSERT_EQUAL_FLOAT(0,fan_led_percent_map(0));
+  
 }
 
 
