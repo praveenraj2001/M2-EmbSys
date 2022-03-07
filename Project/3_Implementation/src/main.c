0461fc267e8b955a79d9fc4c5f8c3522c3d29656
praveenraj2001 <nyalam.praveenraj@gmail.com>
2022-03-07 17:41:11 +0530

    commit

--- Project/3_Implementation/src/main.c
+++ Project/3_Implementation/src/main.c
@@ -6,7 +6,7 @@ int main()
 
 	DDRD |= 0b00001111;									// insted of "DDRD |= 0x0f;"
 	DDRC |= 0b1000000;									// set b6 as output port
-	LCD_Init();											// initilize led
+	LCD_Init();// initilize led
 
 	if (PIND & (1 << 4))
 	{ 													// moniter bit 4 of portd for buzzer (Polling)
