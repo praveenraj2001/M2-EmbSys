6c55af6bda301b67856912c159ec62bc2abee68d
praveenraj2001 <nyalam.praveenraj@gmail.com>
2022-03-07 17:37:15 +0530

    commit

--- Project/3_Implementation/src/main.c
+++ Project/3_Implementation/src/main.c
@@ -4,7 +4,7 @@
 int main()
 {
 
-	DDRD |= 0b00001111; 								// insted of "DDRD |= 0x0f;"
+	DDRD |= 0b00001111;									// insted of "DDRD |= 0x0f;"
 	DDRC |= 0b1000000;									// set b6 as output port
 	LCD_Init();											// initilize led
 
